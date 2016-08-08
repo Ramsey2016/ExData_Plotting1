e3c429aa6cbfca121178d4fafc18e4e40bf0d2f0
Remzi Kurshumliu <remzi04@gmail.com>
2016-08-08 22:40:19 +0200

    Add files via upload

---
+++ call_all.R
@@ -0,0 +1,7 @@
+# This script I designed to simply set the working directory but also generate
+# plots. This was not part of assignment but just wanted to do a bit more and make script generation easier
+setwd("C:\\egraphs");
+source("plot1.R");
+source("plot2.R");
+source("plot3.R");
+source("plot4.R");
